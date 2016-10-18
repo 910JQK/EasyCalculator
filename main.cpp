999db5b9e7379ac1a9dafb9dff9783dcceff2480
910JQK <v910JQK@gmail.com>
2016-10-18 22:19:22 +0800

    Support reading file from argv[]

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
 #include <cstdlib>
 #include "readline.hpp"
@@ -20,29 +21,40 @@ void init_parsers(){
 
 
 void print_help(){
-  std::cout << "Usage: EasyCalculator [options]\n\n";
+  std::cout << "Usage: EasyCalculator [options] [input file]...\n\n";
   std::cout << "  -q, --quiet\tDisable interactive command line.\n";
 }
 
 
-void parse_args(int argc, char **argv){
-  int i;
-  for(i=1; i<argc; i++){
+bool parse_args(int argc, char **argv){
+  bool has_file = false;
+  for(int i=1; i<argc; i++){
     if(!strcmp(argv[i], "-q") || !strcmp(argv[i], "--quiet")){
       quiet = true;
     }else if(!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")){
       print_help();
       exit(0);
     }else{
-      std::cerr << "Invalid argument " << argv[i] << '\n';
+      std::ifstream f(argv[i]);
+      if(f.good()){
+	std::string line;
+	while(std::getline(f, line))
+	  parse_line(line);
+      }else{
+	std::cerr << "Invalid file " << argv[i] << '\n';
+	exit(1);
+      }
+      has_file = true;
     }
   }
+  return has_file;
 }
 
 
 int main(int argc, char **argv){
   init_parsers();
-  parse_args(argc, argv);
+  if(parse_args(argc, argv))
+    return 0;
   if(!quiet){
     rl_readline_name = "EasyCalculator";
     rl_attempted_completion_function = calc_completion;
